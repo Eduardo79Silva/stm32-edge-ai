f0b7c066b708a30546f72d38a7ba744c437f2587
Eduardo Silva <eduardo4silva@gmail.com>
2026-06-11 08:14:21 +0200

    feat: add UART communication based on B1 press

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -229,10 +229,15 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
   if (GPIO_Pin != B1_Pin)
     return;
 
+  uint8_t msg[] = "Hello World\r\n";
+  uint8_t msg2[] = "Bye World\r\n";
+
   if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET) {
     HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+    HAL_UART_Transmit(&huart2, msg, sizeof(msg) - 1, 100);
   } else {
     HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+    HAL_UART_Transmit(&huart2, msg2, sizeof(msg2) - 1, 100);
   }
 }
 
